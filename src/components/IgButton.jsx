03245126f774d351818a9fbe5d4c1f242e87c828
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-20 12:40:33 +0200

    logos

--- src/components/IgButton.jsx
+++ src/components/IgButton.jsx
@@ -4,8 +4,8 @@ const IgButton = () => {
 	return (
 		<div className='ig-btn'>
 		<img src='/assets/logotipo-solete-HORIZONTAL.png' alt='logo' className='w-20 bg-white rounded-md'/>
-			{/* <button
-				className='insta-btn'
+			<button
+				className='insta-btn '
 				onClick={event => {
 					if (!window.confirm('¿Quieres acceder a Instagram?')) {
 						event.preventDefault();
@@ -15,7 +15,7 @@ const IgButton = () => {
 				<a target='_blank' href='https://www.instagram.com/puzzle_madrid/'>
 					<img src='/assets/iconoInstagram.png' alt='ig' />
 				</a>
-			</button> */}
+			</button>
 		</div>
 	);
 };
