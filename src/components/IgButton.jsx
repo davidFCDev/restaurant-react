a3d4709133e7a1b184a53af00eaaa268ff5932e1
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-20 15:44:36 +0200

    estilos

--- src/components/IgButton.jsx
+++ src/components/IgButton.jsx
@@ -4,7 +4,7 @@ const IgButton = () => {
 	return (
 		<div className='ig-btn'>
 		<a href='https://www.guiarepsol.com/es/fichas/solete/puzzle-332370/' target='_blank' className='flex overflow-hidden rounded-md '>
-			<img src='/assets/logotipo-solete-HORIZONTAL.png' alt='logo' className='w-16 md:w-20 bg-white rounded-md hover:scale-110 transition-all'/>
+			<img src='/assets/logotipo-solete-HORIZONTAL.png' alt='logo' className='w-20 md:w-28 bg-white rounded-md hover:scale-110 transition-all'/>
 		</a>
 			<button
 				className='insta-btn '
